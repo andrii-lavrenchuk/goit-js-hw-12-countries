c83601fe939d87e93d6c84b2b86236e0bcae16c1
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-10-30 19:25:51 +0200

    Adds handle errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // import fetchCountries from './js/fetchCountries.js';
 // fetchCountries();
 import countryTmpl from './templates/countryMarkup.hbs';
+import countryListTmpl from './templates/countryList.hbs';
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 const debounce = require('lodash.debounce');
 
 const refs = {
@@ -8,11 +13,11 @@ const refs = {
   serchForm: document.querySelector('.js-search-form'),
 };
 
-refs.serchForm.addEventListener('input', debounce(onFormInput, 500));
+refs.serchForm.addEventListener('input', debounce(onInput, 500));
 
-function onFormInput(e) {
-  let form = e.target;
-  console.log('onFormInput -> form', form);
+function onInput(e) {
+  clearSerch();
+  const form = e.target;
   const countryName = form.value;
 
   fetchCountryByName(countryName)
@@ -31,7 +36,42 @@ function fetchCountryByName(name) {
 }
 
 function renderCountryCard(country) {
-  const markup = countryTmpl(country);
-  refs.countryContainer.innerHTML = markup;
-  refs.serchForm.value = '';
+  console.log('renderCountryCard -> country', country);
+  if (country.length > 10) {
+    onError();
+    return;
+  }
+  if (country.length >= 2 && country.length < 10) {
+    const listMarkup = countryListTmpl(country);
+
+    refs.countryContainer.innerHTML = listMarkup;
+
+    onInfo();
+    return;
+  }
+  if (country.length === 1) {
+    const markup = countryTmpl(country);
+    refs.countryContainer.innerHTML = markup;
+    return;
+  }
+  onInfo();
+}
+
+function clearSerch() {
+  refs.countryContainer.innerHTML = '';
+}
+// console.log(error());
+function onError() {
+  error({
+    title: 'Title',
+    text: 'To many matches found. Please enter a more specific query',
+    delay: 1500,
+  });
+}
+
+function onInfo() {
+  info({
+    text: 'Нема співпадінь',
+    delay: 1000,
+  });
 }
