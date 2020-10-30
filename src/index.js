550ce750f5081c9df8c043ddb66bf5d070c82199
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-10-30 16:07:18 +0200

    Adds country search

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 // import fetchCountries from './js/fetchCountries.js';
 // fetchCountries();
 import countryTmpl from './templates/countryMarkup.hbs';
+const debounce = require('lodash.debounce');
+
 const refs = {
-  countryContainer: document.querySelector('.country-container'),
+  countryContainer: document.querySelector('.js-country-container'),
+  serchForm: document.querySelector('.js-search-form'),
 };
 
-fetch('https://restcountries.eu/rest/v2/name/canada')
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    console.log(country);
-    const markup = countryTmpl(country);
-    console.log('markup', markup);
-    refs.countryContainer.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+refs.serchForm.addEventListener('input', debounce(onFormInput, 500));
+
+function onFormInput(e) {
+  let form = e.target;
+  console.log('onFormInput -> form', form);
+  const countryName = form.value;
+
+  fetchCountryByName(countryName)
+    .then(renderCountryCard)
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function fetchCountryByName(name) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(
+    response => {
+      return response.json();
+    },
+  );
+}
+
+function renderCountryCard(country) {
+  const markup = countryTmpl(country);
+  refs.countryContainer.innerHTML = markup;
+  refs.serchForm.value = '';
+}
